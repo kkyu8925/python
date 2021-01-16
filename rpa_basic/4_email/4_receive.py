fb6cffa0062b11dcde2cee96db662d58fe945aac
gusrb8925 <gusrb8925@naver.com>
2021-01-16 23:59:03 +0900

    add rpa_basic 21-01-16

--- rpa_basic/4_email/4_receive.py
+++ rpa_basic/4_email/4_receive.py
@@ -18,4 +18,16 @@ for msg in mailbox.fetch(limit=1, reverse=True):
     print("HTML 메시지", msg.html)
     pritn("-"*100)
 
+    # 첨부 파일
+    for att in msg.attachments:
+        print("첨부파일 이름", att.filename)
+        print("타입", att.content_type)
+        print("크기", att.size)
+
+        # 파일 다운
+        with open("download_" + att.filename, "wb") as f:
+            f.write(att.payload)
+            print("첨부파일 {} 다운로드 완료".format(att.filename))
+
+
 mailbox.logout()
